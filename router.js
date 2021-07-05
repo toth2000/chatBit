e4eea3d0cc9e64080bc5ac65395ca5707a23c5ec
toth2000 <tothagata.bhattacharjee@gmail.com>
2021-07-05 23:08:37 +0530

    Added Procfile

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,res)=>{
-    res.send('Server is Running');
+    res.send(`Server is Running at port ${process.env.PORT || 5000}`);
 });
 
 module.exports = router
