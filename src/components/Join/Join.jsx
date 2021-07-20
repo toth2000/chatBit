e20245e6db4c4fea1222e8dd2a581618229bf79b
linzof <linzof@outlook.com>
2021-07-20 01:21:57 -0700

    fixed the url in the a tag

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,6 +1,6 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import './Join.css'
+import './Join.css';
 
 const Join = () => {
   const [ name, setName ] = useState('');
